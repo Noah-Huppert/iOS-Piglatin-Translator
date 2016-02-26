50265bc4ce9b86cca3e753eb42ea1b4e0c6d6ba4
Noah Huppert <noahhuppert@gmail.com>
2016-02-26 13:47:31 -0500

    Initial Commit

---
+++ Piglatin Translator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Piglatin Translator
+//
+//  Created by block7 on 2/26/16.
+//  Copyright © 2016 noahhuppert.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
